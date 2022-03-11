8508c4690a21a50558c3b795d4f8dc9dff8257ef
Alexandros <alexandros.stavroulakis@croonwolterendros.nl>
2022-03-11 20:34:45 +0100

    finish refactoring

--- flask_app/application/__init__.py
+++ flask_app/application/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask import current_app
 from .blueprints.auth import auth_bp
-
+from .blueprints.dashboard import dash_bp
+from .blueprints.actions import action_bp
 def create_cam_app(config_file = None):
     
     app = Flask(__name__)
     if config_file:
         app.config.from_object(config_file)
-    print(app.config['DB_HOST'])
+
     app.register_blueprint(auth_bp)
+    app.register_blueprint(dash_bp)
+    app.register_blueprint(action_bp)
 
     return app
--- flask_app/application/helpers/db.py
+++ flask_app/application/helpers/db.py
@@ -3,10 +3,10 @@ from flask import current_app
 
 # connect to the sql db
 def db_connect():
-    connection = mysql.connector.connect(host = current_app.config['db_host'],
-                                         port = current_app.config['db_port'],
-                                         user = current_app.config['db_user'],
-                                         password = current_app.config['db_pass'],
-                                         database = current_app.config['db_name'])
+    connection = mysql.connector.connect(host = current_app.config['DB_HOST'],
+                                         port = current_app.config['DB_PORT'],
+                                         user = current_app.config['DB_USER'],
+                                         password = current_app.config['DB_PASSWORD'],
+                                         database = current_app.config['DB_DATABASE'])
     cursor = connection.cursor(buffered=True , dictionary=True)
-    return cursor
+    return connection, cursor
